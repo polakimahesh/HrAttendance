f84c4b45f7fe0a95bca7acae5cb846ae00272f36
polakimahesh <94633440+polakimahesh@users.noreply.github.com>
2023-03-03 18:14:36 +0530

    Delete src directory

--- src/main/java/com/example/HrAttendance/Users/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.HrAttendance.Users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<Users,Integer> {
-
-}
